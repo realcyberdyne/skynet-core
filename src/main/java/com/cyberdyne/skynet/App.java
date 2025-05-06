ff037a9bf17e823b7ffb0d27bcf3ca6b317f7585
reza farazi <rezafarazi1378@gmail.com>
2025-05-06 23:22:55 +0330

    SkyNet Server is done

---
+++ src/main/java/com/cyberdyne/skynet/App.java
@@ -0,0 +1,20 @@
+package com.cyberdyne.skynet;
+
+import com.cyberdyne.skynet.Services.Config.Config;
+import com.cyberdyne.skynet.Services.VPN.Functions.Proxy;
+import com.cyberdyne.skynet.Services.VPN.Functions.VPNCore;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+
+        //Get config from file
+        new Config();
+
+        //Get begin tunnel
+        new VPNCore(Config.VPNPort,"Hello");
+
+    }
+}
+
